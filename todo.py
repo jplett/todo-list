4223aaeb6a46d8e5a146c2c645a5a02cb1d84d9d
jplett <me@jamesplett.com>
2015-11-06 15:57:10 -0800

    Update todo.py

--- todo.py
+++ todo.py
@@ -29,11 +29,11 @@ def print_all():
 	print ""
 	print_done()
 
-def write_to_file():
+def write_to_file(filename):
 	"""Writes to a user-specified file."""
-	filename = "todo.txt"
-	print "Name the list. Default is %s"%filename
-	filename = raw_input(prompt)
+#	filename = "todo.txt"
+#	print "Name the list. Default is %s"%filename
+#	filename = raw_input(prompt)
 	print "Are you sure you want to write to %s?"%filename
 	print "This will overwrite the file."
 	go = raw_input("Y/N? ")
@@ -55,6 +55,23 @@ def write_to_file():
 
 def read_from_file(todo,done):
 	"""Reads from a designated file and populates a "todo" and "done" list. It *adds* to these lists, so if there are existing items they won't be removed. Allows one to merge lists."""
+	filename = "todo.txt"
+	print "Name the list. Default is %s"%filename
+	filename = raw_input(prompt)
+	try:
+		txt = open(filename)
+		list = txt.readlines()
+		try:
+			for i in range(1,list.index('Done\n')):
+				todos.append(list[i].rstrip('\n'))
+			for i in range(list.index('Done\n')+1,len(list)):
+				done.append(list[i].rstrip('\n'))
+			print "Items added."
+		except ValueError:
+			print "List is invalid. Required 'Done' to be in the list."
+	except EnvironmentError:
+		print "No such file!"
+	
 	
 	
 todos = []
@@ -76,6 +93,23 @@ while run == 1: ## this should be turned into a function or something?
 	if action[0] == "pop" or action[0] == "remove" or action[0] == "finished" or action[0] == "finish":
 		remove((action[1]))
 	if action[0] == "write":
-		write_to_file()
+		write_to_file(action[1])
+	if action[0] == "read":
+		read_from_file(todos,done)
+	if action[0] == "wipe":
+		print "Wiping list."
+		print "Saving backup, just in case!"
+		write_to_file("backup.txt")
+		todos = []
+		done = []
+	if action[0] == "quit":
+		quit = raw_input("Save before quitting? Y/N :")
+		if quit.lower() == "y":
+			write_to_file(raw_input("Please give filename: "))
+		else:
+			pass
+		run = 0
+		
+		
 	print ""
 	
